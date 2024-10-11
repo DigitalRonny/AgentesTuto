6a36b92777798adcc31c3ffe10ebf07bb4034c2e
Ronny Cartagena <rondamian04@gmail.com>
2024-10-11 08:34:50 -0500

    Creación del proyecto Java

---
+++ src/Contenedor.java
@@ -0,0 +1,18 @@
+import jade.core.ProfileImpl;
+import jade.core.Profile;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.StaleProxyException;
+
+public final class Contenedor {
+    private static AgentContainer agentContainer;
+    public static void configurarContentedor(){
+        jade.core.Runtime runtime = jade.core.Runtime.instance();
+        Profile profile = new ProfileImpl(null, 1099, null);
+        agentContainer = runtime.createMainContainer(profile);
+        agregarAgentes();
+    }
+
+    public static void agregarAgentes(){
+    }
+}
+
