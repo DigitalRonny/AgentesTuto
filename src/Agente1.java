df6979859b7e3689f534217bf28413db27e57673
Ronny Cartagena <rondamian04@gmail.com>
2024-10-12 07:03:59 -0500

    Comportamiento simple

--- src/Agente1.java
+++ src/Agente1.java
@@ -1,11 +1,25 @@
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;;
 
 public class Agente1 extends Agent{
     
     @Override
     protected void setup(){
-        System.out.println("Hola");
-        System.out.println("Mi nombre es: " + getLocalName());
-        System.out.println("Mi AID es: " + getAID());
+        addBehaviour(new Comportamiento());    
+    }
+
+    public class Comportamiento extends Behaviour{
+
+        @Override
+        public void action() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Contador en: "+ i);
+            }
+        }
+
+        @Override
+        public boolean done() {
+            System.out.println("FIN");
+            return true;        } 
     }
 }
